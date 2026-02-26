5fbc963cd469885b657ce156b67966098ec7a592
Rahi6023 <rahidulmowlarahi6023@gmail.com>
2026-02-26 13:37:32 +0600

    Project Finished

--- index.js
+++ index.js
@@ -3,15 +3,28 @@ const enableList1 = document.getElementById("enableList1")
 const enableList2 = document.getElementById("enableList2")
 const list1 = document.getElementById('list1')
 const list2 = document.getElementById('list2')
-const convertBtn = document.getElementById('convert-btn')
 const swapBtn = document.getElementById('swap-btn')
-const convertFrom = document.getElementById('convertFrom')
-const convertTo = document.getElementById('convertTo')
 const countryFrom = document.getElementById('countryFrom')
 const countryTo = document.getElementById('countryTo')
 const input = document.getElementById('amount')
 const convertedPrice = document.getElementById('converted-price')
+const today = new Date()
+const day = today.getDate()
+const month = today.getMonth() +1
+const year = today.getFullYear()
+const nav = document.querySelector('nav')
+const URL = "https://open.er-api.com/v6/latest/USD"
+const resultArea = document.getElementById('converted-price')
 
+console.log(day,month,year)
+
+const DateHtml = `
+                <span id="Date">
+                    <i class="fa-regular fa-calendar"></i>
+                    ${day}/${month}/${year}
+                </span>
+                `
+nav.innerHTML = DateHtml
 
 enableList1.addEventListener("click",()=>{
     list1.classList.toggle('hidden')
@@ -23,7 +36,7 @@ enableList2.addEventListener("click",()=>{
 })
 
 
-list1.addEventListener("click",(e)=>{
+list1.addEventListener("click",async (e)=>{
     const selectedItem = e.target.closest('li')
     const selectedCountry = selectedItem.dataset.countrytag
     countryFrom.innerHTML = `
@@ -32,8 +45,10 @@ list1.addEventListener("click",(e)=>{
     `
     list1.classList.toggle('hidden')
     enableList1.classList.toggle('rotate')
+    let result = await getConverted(Number(input.value),countryFrom.innerText,countryTo.innerText)
+    resultArea.innerText = result
 })
-list2.addEventListener("click",(e)=>{
+list2.addEventListener("click",async (e)=>{
     const selectedItem2 = e.target.closest('li')
     const selectedCountry2 = selectedItem2.dataset.countrytag
     countryTo.innerHTML = `
@@ -42,16 +57,27 @@ list2.addEventListener("click",(e)=>{
     `
     list2.classList.toggle('hidden')
     enableList2.classList.toggle('rotate')
+    let result = await getConverted(Number(input.value),countryFrom.innerText,countryTo.innerText)
+    resultArea.innerText = result
 })
 
-convertBtn.addEventListener("click",()=>{
+async function getConverted(amount,from,to){
+    let response = await fetch(URL)
+    let usableData = await response.json()
+    return amount/usableData.rates[from] *usableData.rates[to]
+}
 
+input.addEventListener("input",async ()=>{
+    let result = await getConverted(Number(input.value),countryFrom.innerText,countryTo.innerText)
+    resultArea.innerText = result
 })
 
 swapBtn.addEventListener("click",()=>{
     let temp = input.value
     input.value = convertedPrice.textContent
-    convertedPrice.textContent = temp
+    if(temp==="")
+    convertedPrice.textContent = "0"
+    else convertedPrice.textContent = temp
     temp = countryFrom.innerHTML
     countryFrom.innerHTML = countryTo.innerHTML
     countryTo.innerHTML = temp
